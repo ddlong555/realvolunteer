bb15615e82d2fbc03aa4258d6bdcaf8a2f7abb92
long <10185101114@ecnu.edu.stu.cn>
2021-04-14 09:45:41 +0800

    睡觉

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
     devServer: {
         open: true, //是否自动弹出浏览器页面
         host: "localhost",
-        port: '8080',
+        port: '1219',
         https: false,
         hotOnly: false,
         proxy: {
